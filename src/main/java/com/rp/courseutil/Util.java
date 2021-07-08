06e4c6f80be1e40633b253aa20b777b3e9edcb1b
reidomath <mat.s.ferreira@gmail.com>
2021-07-08 00:57:15 -0300

    [Mono] - Emitting Empty / Error

--- src/main/java/com/rp/courseutil/Util.java
+++ src/main/java/com/rp/courseutil/Util.java
@@ -8,20 +8,19 @@ public class Util {
 
     private static final Faker FAKER = Faker.instance();
 
-
     public static Consumer<Object> onNext() {
         return o -> System.out.println("Received: " + o);
     }
 
     public static Consumer<Throwable> onError() {
-        return e -> System.out.println("Received: " + e.getMessage());
+        return e -> System.out.println("ERROR: " + e.getMessage());
     }
 
-    public static Runnable onComplete(){
+    public static Runnable onComplete() {
         return () -> System.out.println("Completed");
     }
 
-    public static Faker faker(){
+    public static Faker faker() {
         return FAKER;
     }
 
