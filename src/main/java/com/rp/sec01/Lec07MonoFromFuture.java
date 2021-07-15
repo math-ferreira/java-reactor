ffb81088760e3b9cb406fa89b87415d005f3c830
math-ferreira <mat.s.ferreira@gmail.com>
2021-07-14 20:53:20 -0700

    [Mono] - From Future

---
+++ src/main/java/com/rp/sec01/Lec07MonoFromFuture.java
@@ -0,0 +1,20 @@
+package com.rp.sec01;
+
+import com.rp.courseutil.Util;
+import reactor.core.publisher.Mono;
+
+import java.util.concurrent.CompletableFuture;
+
+public class Lec07MonoFromFuture {
+
+    public static void main(String[] args) {
+        Mono.fromFuture(getName())
+                .subscribe(Util.onNext());
+
+        Util.sleepSeconds(1);
+    }
+
+    public static CompletableFuture<String> getName() {
+        return CompletableFuture.supplyAsync(() -> Util.faker().name().fullName());
+    }
+}
