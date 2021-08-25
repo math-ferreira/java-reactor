414698e0515832517d7008fa499c5c97eed2a09a
math-ferreira <mat.s.ferreira@gmail.com>
2021-08-24 18:18:52 -0700

    [Mono] - From Runnable

--- src/main/java/com/rp/sec01/Lec08MonoFromRunnable.java
+++ src/main/java/com/rp/sec01/Lec08MonoFromRunnable.java
@@ -8,16 +8,15 @@ public class Lec08MonoFromRunnable {
     public static void main(String[] args) {
 
         Mono.fromRunnable(timeConsumingProcess())
-                .subscribe(
-                        Util.onNext(),
+                .subscribe(Util.onNext(),
                         Util.onError(),
                         () -> {
-                            System.out.println("Process is done. Sending emails...");
-                        }
-                );
+                            System.out.println("process is done. Sending emails...");
+                        });
+
     }
 
-    public static Runnable timeConsumingProcess() {
+    private static Runnable timeConsumingProcess() {
         return () -> {
             Util.sleepSeconds(3);
             System.out.println("Operation completed");
